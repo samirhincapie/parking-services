d26c6ea624bae93d364ca778e3bfc11d9e9b9fc0
samirhincapie <samir.hincapie@ceiba.com.co>
2017-10-12 15:51:16 -0500

    Corrección de seguridad

--- src/main/java/co/com/ceiba/model/VigilanteException.java
+++ src/main/java/co/com/ceiba/model/VigilanteException.java
@@ -1,7 +1,7 @@
 package co.com.ceiba.model;
 
 public class VigilanteException extends Exception {
-	public static String NO_HAY_PARQUEO_DISPONIBLE_PARA_CARRO = "No hay parqueo disponible para carro";
+	public static final String NO_HAY_PARQUEO_DISPONIBLE_PARA_CARRO = "No hay parqueo disponible para carro";
 	
 	public VigilanteException(String mensaje){
 		super(mensaje);
