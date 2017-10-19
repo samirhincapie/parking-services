cd0011258ca9af2ebc9f7cd01eebb66a2aac5ce9
samirhincapie <samir.hincapie@ceiba.com.co>
2017-10-19 13:35:28 -0500

    Pruebas unitarias

--- src/test/java/co/com/ceiba/test/persistencia/builder/ParqueoCarroBuilderTest.java
+++ src/test/java/co/com/ceiba/test/persistencia/builder/ParqueoCarroBuilderTest.java
@@ -1,5 +1,47 @@
 package co.com.ceiba.test.persistencia.builder;
 
-public class ParqueoCarroBuilderTest {
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import co.com.ceiba.model.Carro;
+import co.com.ceiba.model.Parqueo;
+import co.com.ceiba.model.Vehiculo;
+import co.com.ceiba.persistencia.builder.ParqueoCarroBuilder;
+import co.com.ceiba.persistencia.entidad.ParqueoEntity;
+
+public class ParqueoCarroBuilderTest {
+	private static final int ID = 1;
+	private static final double VALOR_ADICIONAL = 0;
+	private static final double VALOR_DIA = 8000;
+	private static final double VALOR_HORA = 1000;
+	private static final String PLACA = "AAA000";
+	
+	private ParqueoEntity mockParqueoEntity;
+	private Parqueo mockParqueo;
+	
+	@Before
+	public void setup(){
+		this.mockParqueo = mock(Parqueo.class);
+		this.mockParqueoEntity = mock(ParqueoEntity.class);
+	}
+
+	@Test
+	public void test(){
+		//Arrange
+		ParqueoCarroBuilder parqueoCarroBuilder = new ParqueoCarroBuilder();		
+		
+		
+		//Act
+		Parqueo parqueo = parqueoCarroBuilder.convertirADominio(this.mockParqueoEntity);		
+		
+		
+		//Assert
+		assertEquals(VALOR_ADICIONAL, parqueo.getValorAdicional());
+		assertEquals(VALOR_DIA, parqueo.getValorDia());
+		assertEquals(VALOR_HORA, parqueo.getValorHora());
+		assertEquals(PLACA, parqueo.getVehiculo().getPlaca());
+	}
 }
